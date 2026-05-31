970eb0cc33c494ce955943a37453b89eaed5f565
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2026-05-31 18:54:07 +0200

    Worst thing ever

--- Formula/karls-gitflow.rb
+++ Formula/karls-gitflow.rb
@@ -1,14 +1,14 @@
 class KarlsGitflow < Formula
   desc "Opinionated Git Flow implementation written in .NET"
   homepage "https://github.com/karl-sjogren/karls-gitflow"
-  version "0.0.13"
+  version "0.0.14"
   license "MIT"
   version_scheme 1
 
-  @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.13/karls-gitflow-0.0.13-osx-arm64.zip"
-  @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.13/karls-gitflow-0.0.13-osx-x64.zip"
-  @arm64sha256 = "c20e79d97701f82d36d88c905fd2ad688b8b082a920a189c8c1f551ee4459030"
-  @x64sha256 = "b1f812f0e91d658f1636e3a958c40610aa6be5a58799ddcae0a2a1538c6111d2"
+  @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-arm64.zip"
+  @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-x64.zip"
+  @arm64sha256 = "00b3934c8ae570b563c1ff30647112a92818bdaa6cc3c5a316b7c100bfc4e101"
+  @x64sha256 = "27a1a8999161dbe4ca0d6c62f811266b169f77dd1f16bc87a8880aacb8ca6254"
 
   if Hardware::CPU.intel?
     url @x64url
@@ -27,6 +27,6 @@ class KarlsGitflow < Formula
   end
 
   test do
-    assert_equal "0.0.13", shell_output("#{bin}/git-flow --version").strip
+    assert_equal "0.0.14", shell_output("#{bin}/git-flow --version").strip
   end
 end
