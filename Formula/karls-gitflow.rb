249b76b0ec65b8e70fcdc80455006093fb7c278e
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2026-08-28 11:04:50 +0200

    Bump version to 0.0.14 and update URLs

--- Formula/karls-gitflow.rb
+++ Formula/karls-gitflow.rb
@@ -1,20 +1,20 @@
 class KarlsGitflow < Formula
   desc "Opinionated Git Flow implementation written in .NET"
   homepage "https://github.com/karl-sjogren/karls-gitflow"
-  version "0.0.13"
+  version "0.0.14"
   license "MIT"
   version_scheme 1
 
   bottle do
-    root_url "https://github.com/karl-sjogren/homebrew-tap/releases/download/karls-gitflow-0.0.13"
+    root_url "https://github.com/karl-sjogren/homebrew-tap/releases/download/karls-gitflow-0.0.14"
     sha256 cellar: :any_skip_relocation, arm64_tahoe: "8df1c6280e4beb9dc88179c0b60a478c6617db63f227d42a7cc2a099451d0088"
     sha256 cellar: :any_skip_relocation, sequoia:     "d461ebfb0c17c12c87f7462fc77549173d3788ac8d58b7ed446386b3f41d6e44"
   end
 
-  @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.13/karls-gitflow-0.0.13-osx-arm64.zip"
-  @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.13/karls-gitflow-0.0.13-osx-x64.zip"
-  @arm64sha256 = "c20e79d97701f82d36d88c905fd2ad688b8b082a920a189c8c1f551ee4459030"
-  @x64sha256 = "b1f812f0e91d658f1636e3a958c40610aa6be5a58799ddcae0a2a1538c6111d2"
+  @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-arm64.zip"
+  @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-x64.zip"
+  @arm64sha256 = "00b3934c8ae570b563c1ff30647112a92818bdaa6cc3c5a316b7c100bfc4e101"
+  @x64sha256 = "b115ed8ce3a2ac3a4c089b92fc462342f75fdfa8e1201186cf3f5d1f01af9c7d"
 
   if Hardware::CPU.intel?
     url @x64url
@@ -38,6 +38,6 @@ class KarlsGitflow < Formula
   end
 
   test do
-    assert_equal "0.0.13", shell_output("#{bin}/git-flow --version").strip
+    assert_equal "0.0.14", shell_output("#{bin}/git-flow --version").strip
   end
 end
