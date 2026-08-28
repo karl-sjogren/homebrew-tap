e12a436f181988cb03dc91ac1b6d1cccf5943776
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2026-08-28 12:09:05 +0200

    Refactor bottle block and remove duplicate URLs

--- Formula/karls-gitflow.rb
+++ Formula/karls-gitflow.rb
@@ -5,18 +5,18 @@ class KarlsGitflow < Formula
   license "MIT"
   version_scheme 1
 
+  bottle do
+    root_url "https://github.com/karl-sjogren/homebrew-tap/releases/download/karls-gitflow-0.0.14"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "8df1c6280e4beb9dc88179c0b60a478c6617db63f227d42a7cc2a099451d0088"
+    sha256 cellar: :any_skip_relocation, sequoia:     "d461ebfb0c17c12c87f7462fc77549173d3788ac8d58b7ed446386b3f41d6e44"
+  end
+
+  @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-arm64.zip"
+  @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-x64.zip"
+  @arm64sha256 = "00b3934c8ae570b563c1ff30647112a92818bdaa6cc3c5a316b7c100bfc4e101"
+  @x64sha256 = "b115ed8ce3a2ac3a4c089b92fc462342f75fdfa8e1201186cf3f5d1f01af9c7d"
+  
   on_macos do
-    bottle do
-      root_url "https://github.com/karl-sjogren/homebrew-tap/releases/download/karls-gitflow-0.0.14"
-      sha256 cellar: :any_skip_relocation, arm64_tahoe: "8df1c6280e4beb9dc88179c0b60a478c6617db63f227d42a7cc2a099451d0088"
-      sha256 cellar: :any_skip_relocation, sequoia:     "d461ebfb0c17c12c87f7462fc77549173d3788ac8d58b7ed446386b3f41d6e44"
-    end
-  
-    @arm64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-arm64.zip"
-    @x64url = "https://github.com/karl-sjogren/karls-gitflow/releases/download/0.0.14/karls-gitflow-0.0.14-osx-x64.zip"
-    @arm64sha256 = "00b3934c8ae570b563c1ff30647112a92818bdaa6cc3c5a316b7c100bfc4e101"
-    @x64sha256 = "b115ed8ce3a2ac3a4c089b92fc462342f75fdfa8e1201186cf3f5d1f01af9c7d"
-  
     if Hardware::CPU.intel?
       url @x64url
       sha256 @x64sha256
